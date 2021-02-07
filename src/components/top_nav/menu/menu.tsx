12e3bf91a68c6cd31c289291de13ee2aaa2364cd
DangaRanga <jasonblue1626@gmail.com>
2021-02-07 11:28:18 -0500

    Made nav toggleable

--- src/components/top_nav/menu/menu.tsx
+++ src/components/top_nav/menu/menu.tsx
@@ -1,16 +1,52 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import "./menu.css";
 import * as icons from "../../icons/icons";
 import colours from "../../../global/colours";
+import { useState } from "react";
 
 interface MenuProps {}
 
 interface MenuState {}
 
-export class Menu extends Component<MenuProps, MenuState> {
-  render() {
-    return <div>{icons.Menu(colours.primary, "24px")}</div>;
-  }
+function Menu() {
+  const [openNav, navToggle] = useState(false);
+
+  const handleNavToggle = () => {
+    navToggle(!openNav);
+  };
+
+  // Appends the active class to the nav items
+  const renderClasses = () => {
+    let classes = "nav-items";
+    if (openNav) {
+      classes += " active";
+    }
+    return classes;
+  };
+
+  return (
+    <div>
+      <div onClick={handleNavToggle}>{icons.Menu(colours.primary, "24px")}</div>
+      <ul className={renderClasses()}>
+        <Link to="/">
+          <li>Home</li>
+        </Link>
+        <Link to="/seach">
+          <li>Search</li>
+        </Link>
+        <Link to="/banks">
+          <li>Nearby Banks</li>
+        </Link>
+        <Link to="/login">
+          <li>Login</li>
+        </Link>
+        <Link to="/signup">
+          <li>Signup</li>
+        </Link>
+      </ul>
+    </div>
+  );
 }
 
 export default Menu;
