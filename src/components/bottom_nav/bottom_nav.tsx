62cd73df7427b33ef69db7837f182873bdf805db
richtwin567 <anakai.richards@gmail.com>
2021-02-07 09:22:51 -0500

    add homepage link

--- src/components/bottom_nav/bottom_nav.tsx
+++ src/components/bottom_nav/bottom_nav.tsx
@@ -2,28 +2,24 @@ import React, { Component } from "react";
 import "./bottom_nav.css";
 import * as icons from "../icons/icons";
 import colours from "../../global/colours";
+import { Link } from "react-router-dom";
 
+interface BottomNavProps {}
 
-interface BottomNavProps {
-    
-}
+interface BottomNavState {}
 
-interface BottomNavState {
-    
-}
-
-export class BottomNav extends Component<BottomNavProps,BottomNavState> {
-    render() {
-        return (
-            <div id="bottom-nav">
-                {icons.Favourite(colours.grey4, "24px")}
-                <div id="feature-home">
-                {icons.Home(colours.onDark, "36px")}
-                </div>
-                {icons.Settings(colours.grey4, "24px")}
-            </div>
-        );
-    }
+export class BottomNav extends Component<BottomNavProps, BottomNavState> {
+	render() {
+		return (
+			<div id="bottom-nav">
+				{icons.Favourite(colours.grey4, "24px")}
+				<div id="feature-home">
+					<Link to="/home">{icons.Home(colours.onDark, "36px")}</Link>
+				</div>
+				{icons.Settings(colours.grey4, "24px")}
+			</div>
+		);
+	}
 }
 
 export default BottomNav;
