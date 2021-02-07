b1cbc9cdec089a9ea0205471c6b0b0f96173878e
DangaRanga <jasonblue1626@gmail.com>
2021-02-07 11:43:53 -0500

    Added bank url to bank search

--- src/pages/bank_search/BankSearch.tsx
+++ src/pages/bank_search/BankSearch.tsx
@@ -15,37 +15,37 @@ import bank3 from "../../assets/bank3.png";
 import banksData from "../../data/bankData.json";
 
 function BankSearch() {
-	var featuredBanks = [];
+  var featuredBanks = [];
 
-	for (const bank of banksData) {
-		featuredBanks.push(
-			<ListTile>
-				<img className="bank-pic" src={bank1} alt="" />
-				<p className="bank-name">{bank.name}</p>
-				<StarRating />
-				<p className="bank-address">189 West 10th St, New York</p>
-				<Button class="call-bank" text="Call" variant="primary" />
-				<Button class="message-bank" text="Message" variant="primary" />
-				<Button class="more-info" text="Open" variant="accent" />
-				<p className="distance">10 min drive</p>
-			</ListTile>
-		);
-	}
-	return (
-		<div id="bank-search">
-			<TopNav />
-			<div id="content">
-				<BankMap></BankMap>
-				<h2>Featured Banks</h2>
-				<List
-					direction="vertical"
-					class="featured-banks"
-					elements={featuredBanks}
-				/>
-			</div>
-			<BottomNav />
-		</div>
-	);
+  for (const bank of banksData) {
+    featuredBanks.push(
+      <ListTile>
+        <img className="bank-pic" src={bank.imgUrl} alt="" />
+        <p className="bank-name">{bank.name}</p>
+        <StarRating />
+        <p className="bank-address">189 West 10th St, New York</p>
+        <Button class="call-bank" text="Call" variant="primary" />
+        <Button class="message-bank" text="Message" variant="primary" />
+        <Button class="more-info" text="Open" variant="accent" />
+        <p className="distance">10 min drive</p>
+      </ListTile>
+    );
+  }
+  return (
+    <div id="bank-search">
+      <TopNav />
+      <div id="content">
+        <BankMap></BankMap>
+        <h2>Featured Banks</h2>
+        <List
+          direction="vertical"
+          class="featured-banks"
+          elements={featuredBanks}
+        />
+      </div>
+      <BottomNav />
+    </div>
+  );
 }
 
 export default BankSearch;
