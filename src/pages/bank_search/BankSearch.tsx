40e2d04e2fe5b3f5947d9c498f7b8b1ab2807a69
richtwin567 <anakai.richards@gmail.com>
2021-02-07 11:22:41 -0500

    merge changes

--- src/pages/bank_search/BankSearch.tsx
+++ src/pages/bank_search/BankSearch.tsx
@@ -12,46 +12,31 @@ import "./bank_search.css";
 import bank1 from "../../assets/bank1.png";
 import bank2 from "../../assets/bank2.png";
 import bank3 from "../../assets/bank3.png";
+import banksData from "../../data/bankData.json";
 
 function BankSearch() {
-	const featuredBanks = [
-		<ListTile>
-			<img className="bank-pic" src={bank1} alt="" />
-			<p className="bank-name">Big Bank Ltd</p>
-			<StarRating />
-			<p className="bank-address">189 West 10th St, New York</p>
-			<Button class="call-bank" text="Call" variant="primary" />
-			<Button class="message-bank" text="Message" variant="primary" />
-			<Button class="more-info" text="Open" variant="accent" />
-			<p className="distance">10 min drive</p>
-		</ListTile>,
-		<ListTile>
-			<img className="bank-pic" src={bank2} alt="" />
-			<p className="bank-name">Capital Bank Ltd</p>
-			<StarRating />
-			<p className="bank-address">189 West 10th St, New York</p>
-			<Button class="call-bank" text="Call" variant="primary" />
-			<Button class="message-bank" text="Message" variant="primary" />
-			<Button class="more-info" text="Open" variant="accent" />
-			<p className="distance">5 min drive</p>
-		</ListTile>,
-		<ListTile>
-			<img className="bank-pic" src={bank3} alt="" />
-			<p className="bank-name">Creative Savings Ltd</p>
-			<StarRating />
-			<p className="bank-address">189 West 10th St, New York</p>
-			<Button class="call-bank" text="Call" variant="primary" />
-			<Button class="message-bank" text="Message" variant="primary" />
-			<Button class="more-info" text="Open" variant="accent" />
-			<p className="distance">20 min drive</p>
-		</ListTile>,
-	];
+	var featuredBanks = [];
+
+	for (const bank of banksData) {
+		featuredBanks.push(
+			<ListTile>
+				<img className="bank-pic" src={bank1} alt="" />
+				<p className="bank-name">{bank.name}</p>
+				<StarRating />
+				<p className="bank-address">189 West 10th St, New York</p>
+				<Button class="call-bank" text="Call" variant="primary" />
+				<Button class="message-bank" text="Message" variant="primary" />
+				<Button class="more-info" text="Open" variant="accent" />
+				<p className="distance">10 min drive</p>
+			</ListTile>
+		);
+	}
 	return (
 		<div id="bank-search">
 			<TopNav />
 			<div id="content">
 				<BankMap></BankMap>
-        <h2>Featured Banks</h2>
+				<h2>Featured Banks</h2>
 				<List
 					direction="vertical"
 					class="featured-banks"
